c1d4735afab5c41049df2178a0dbefb30d31a67b
christian-bromann <contact@christian-bromann.com>
2014-08-12 19:49:36 -0700

    init db via python script

--- init_db.py
+++ init_db.py
@@ -1,5 +1,4 @@
 import os
-import nose
 
 parentdir = os.path.dirname(os.path.abspath(__file__))
 os.sys.path.insert(0, parentdir)
@@ -8,5 +7,3 @@ import flaskr
 
 with flaskr.app.app_context():
             flaskr.init_db()
-
-nose.run()
